43bc6b9cef23b8d92dde42f41210f200eeeb1f4c
CRATETV <165331977+CRATETV@users.noreply.github.com>
2026-08-25 07:46:32 -0400

    Hide The Unpack (zine) from navigation while it's still a work in progress

--- components/Header.tsx
+++ components/Header.tsx
@@ -71,7 +71,6 @@ const Header: React.FC<HeaderProps> = ({
     
     const navLinks = [
         { path: '/', label: 'Home' },
-        { path: '/zine', label: 'The Unpack' },
         { path: '/public-square', label: 'Public Square' }
     ];
     if (user && (user.isActor || user.isFilmmaker)) navLinks.push({ path: '/portal', label: 'Creator Hub' });
